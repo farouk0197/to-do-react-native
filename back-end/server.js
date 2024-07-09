7145f6abfd6d8a3ea3cabc0d67106e5e8cc5a491
farouk0197 <fkatakpaou@gmail.com>
2024-07-08 22:06:58 -0400

    vercel

--- back-end/server.js
+++ back-end/server.js
@@ -14,6 +14,7 @@ app.get('/items', async (req, res) => {
     try{
         const items = await Item.find({})
         res.status(200).json(items)
+        console.log(items)
     } catch (err){
         res.status(500).json({message: err.message})
         console.log(err.message)
@@ -27,7 +28,6 @@ app.post('/create', async (req, res) => {
     try{
         const createdItem = await Item.create(req.body)
         res.status(200).json(createdItem)
-        console.log(createdItem)
     } catch (err){
         res.status(500).json({message: err.message})
         console.log(err.message)
